1177f0cb351bf20a376c5184623044ce881ce00c
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-10-25 11:21:01 -0500

    init tasks

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "DashBoard",
+}
+</script>
+
+<template>
+<section>
+  
+  <div id="select-emotion-b">
+
+  </div>
+  
+  <div id="section-emotion">
+    <div id="dominant-emotion-b">
+      
+    </div>
+    
+    <div id="circular-graph-b">
+      
+    </div>
+    
+  </div>
+  
+  <div id="chart-emotion-area-b">
+    
+  </div>
+  
+  <div id="feature-emotion-trend-b">
+    
+  </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
