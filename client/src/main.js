c52769c24a54a7ef52f77635c49d6d9cc1a303d7
FabiolaGuardamin <85091456+FabiolaGuardamin@users.noreply.github.com>
2023-10-27 12:47:23 -0500

    Add Notification

--- client/src/main.js
+++ client/src/main.js
@@ -3,34 +3,35 @@ import App from "./App.vue";
 import router from "./routes/index.js";
 import './assets/content.css'
 import './assets/dashboard.css'
+import './assets/sidebar.css'
 
-const app = createApp(App);
+// const app = createApp(App);
 
-const window = document.getElementById('ventanaCita');
-const iconoCampana = document.getElementById('icono-campana');
-const closewindowButton = document.getElementById('closewindow');
-const miembrosPendientes = document.getElementById('miembrosPendientes');
+// const window = document.getElementById('ventanaCita');
+// const iconoCampana = document.getElementById('icono-campana');
+// const closewindowButton = document.getElementById('closewindow');
+// const miembrosPendientes = document.getElementById('miembrosPendientes');
 
-function abrirVentana(citasPendientes) {
-  miembrosPendientes.innerHTML = '';
-  citasPendientes.forEach((cita) => {
-    const li = document.createElement('li');
-    li.textContent = cita;
-    miembrosPendientes.appendChild(li);
-  });
+// function abrirVentana(citasPendientes) {
+//   miembrosPendientes.innerHTML = '';
+//   citasPendientes.forEach((cita) => {
+//     const li = document.createElement('li');
+//     li.textContent = cita;
+//     miembrosPendientes.appendChild(li);
+//   });
 
-  window.style.display = 'block';
-}
+//   window.style.display = 'block';
+// }
 
-function cerrarwindow() {
-    window.style.display = 'none';
-}
+// function cerrarwindow() {
+//     window.style.display = 'none';
+// }
 
-iconoCampana.addEventListener('click', abrirVentana);
-closewindowButton.addEventListener('click', cerrarwindow);
+// iconoCampana.addEventListener('click', abrirVentana);
+// closewindowButton.addEventListener('click', cerrarwindow);
 
-const citasPendientes = ["Margiory", "Marcela", "Milloshy", "Fabiola", "Adrian"]; //aqui se reemplazaría con la data que tengamos
-abrirVentana(citasPendientes);
+// const citasPendientes = ["Margiory", "Marcela", "Milloshy", "Fabiola", "Adrian"]; //aqui se reemplazaría con la data que tengamos
+// abrirVentana(citasPendientes);
 
 
 app.use(router);
